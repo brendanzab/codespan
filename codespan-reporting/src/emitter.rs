fffbb382086c3a97a8030130939fdc5a1d2965bf
Markus Westerlind <marwes91@gmail.com>
2018-04-11 23:31:58 +0200

    Display label messages inline with the marker

--- codespan-reporting/src/emitter.rs
+++ codespan-reporting/src/emitter.rs
@@ -11,8 +11,12 @@ where
     W: WriteColor,
 {
     let supports_color = writer.supports_color();
+    let line_location_color = ColorSpec::new().set_fg(Some(Color::Cyan)).clone();
+    let diagnostic_color = ColorSpec::new()
+        .set_fg(Some(diagnostic.severity.color()))
+        .clone();
 
-    writer.set_color(ColorSpec::new().set_fg(Some(diagnostic.severity.color())))?;
+    writer.set_color(&diagnostic_color)?;
     write!(writer, "{}", diagnostic.severity)?;
     writer.reset()?;
     writeln!(writer, ": {}", diagnostic.message)?;
@@ -37,33 +41,49 @@ where
                     .expect("line_prefix");
                 let line_marked = file.src_slice(label.span).expect("line_marked");
                 let line_suffix = file.src_slice(line_span.with_start(label.span.end()))
-                    .expect("line_suffix");
+                    .expect("line_suffix")
+                    .trim_right_matches(|c: char| c == '\r' || c == '\n');
 
-                writer.set_color(ColorSpec::new().set_fg(Some(Color::Cyan)))?;
-                let line_location_prefix = format!("{} | ", line.number());
-                write!(writer, "{}", line_location_prefix)?;
+                writer.set_color(&line_location_color)?;
+                let line_string = line.number().to_string();
+                let line_location_prefix = format!("{:prefix$} | ", "", prefix = line_string.len());
+                write!(writer, "{} | ", line_string)?;
                 writer.reset()?;
 
                 write!(writer, "{}", line_prefix)?;
-                writer.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
+                writer.set_color(&diagnostic_color)?;
                 write!(writer, "{}", line_marked)?;
                 writer.reset()?;
-                write!(writer, "{}", line_suffix)?;
+                writeln!(writer, "{}", line_suffix)?;
 
-                if !supports_color {
-                    writeln!(
+                if !supports_color || label.message.is_some() {
+                    writer.set_color(&line_location_color)?;
+                    write!(writer, "{}", line_location_prefix)?;
+                    writer.reset()?;
+
+                    writer.set_color(&diagnostic_color)?;
+                    write!(
                         writer,
                         "{:prefix$}{:^>marked$}",
                         "",
                         "",
-                        prefix = line_location_prefix.len() + line_prefix.len(),
+                        prefix = line_prefix.len(),
                         marked = line_marked.len()
                     )?;
+                    writer.reset()?;
+
+                    if label.message.is_none() {
+                        writeln!(writer)?;
+                    }
                 }
 
                 match label.message {
-                    None => writeln!(writer)?,
-                    Some(ref label) => writeln!(writer, ": {}", label)?,
+                    None => (),
+                    Some(ref label) => {
+                        writer.set_color(&diagnostic_color)?;
+                        writeln!(writer, " {}", label)?;
+                        writer.reset()?;
+                    },
                 }
             },
         }
