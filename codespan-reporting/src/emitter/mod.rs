aa38ed8d0576a9340f5bed8e572eba56f3690c89
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-06-10 17:05:54 +1000

    Add examples to DisplayStyle doc comments

--- codespan-reporting/src/emitter/mod.rs
+++ codespan-reporting/src/emitter/mod.rs
@@ -6,12 +6,28 @@ use crate::{Diagnostic, Severity};
 
 mod views;
 
-/// The display style to use.
+/// The display style to use when rendering diagnostics.
 #[derive(Clone, Debug)]
 pub enum DisplayStyle {
     /// Output a richly formatted diagnostic, with source code previews.
+    ///
+    /// ```text
+    /// error[E0001]: unexpected type in `+` application
+    ///
+    ///    ┌── test:2:9 ───
+    ///    │
+    ///  2 │ (+ test "")
+    ///    │         ^^ expected `Int` but found `String`
+    ///    │
+    ///    = expected type `Int`
+    ///         found type `String`
+    /// ```
     Rich,
     /// Output a short diagnostic, with a line number, severity, and message.
+    ///
+    /// ```text
+    /// test:2:9: error[E0001]: unexpected type in `+` application
+    /// ```
     Short,
 }
 
