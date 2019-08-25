422a72aa4d8ca7322dd9053edb504431d55e55fa
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-08-25 22:11:43 +1000

    Group diagnostics by file

--- codespan-reporting/src/term/views/diagnostic.rs
+++ codespan-reporting/src/term/views/diagnostic.rs
@@ -19,14 +19,42 @@ impl<'a> RichDiagnostic<'a> {
     }
 
     pub fn emit(&self, writer: &mut impl WriteColor, config: &Config) -> io::Result<()> {
+        use std::collections::BTreeMap;
+
+        use super::MarkStyle;
+
         Header::new(self.diagnostic).emit(writer, config)?;
         NewLine::new().emit(writer, config)?;
 
-        SourceSnippet::new_primary(self.files, &self.diagnostic).emit(writer, config)?;
+        let primary_label = &self.diagnostic.primary_label;
+        let primary_file_id = self.diagnostic.primary_label.file_id;
+        let severity = self.diagnostic.severity;
+        let notes = &self.diagnostic.notes;
+
+        // Group labels by file
+
+        let mut label_groups = BTreeMap::new();
+
+        label_groups
+            .entry(primary_file_id)
+            .or_insert(vec![])
+            .push((primary_label, MarkStyle::Primary(severity)));
+
+        for secondary_label in &self.diagnostic.secondary_labels {
+            label_groups
+                .entry(secondary_label.file_id)
+                .or_insert(vec![])
+                .push((secondary_label, MarkStyle::Secondary));
+        }
+
+        // Emit the snippets, starting with the one that contains the primary label
+
+        let labels = label_groups.remove(&primary_file_id).unwrap_or(vec![]);
+        SourceSnippet::new(self.files, primary_file_id, labels, notes).emit(writer, config)?;
         NewLine::new().emit(writer, config)?;
 
-        for label in &self.diagnostic.secondary_labels {
-            SourceSnippet::new_secondary(self.files, &label).emit(writer, config)?;
+        for (file_id, labels) in label_groups {
+            SourceSnippet::new(self.files, file_id, labels, &[]).emit(writer, config)?;
             NewLine::new().emit(writer, config)?;
         }
 
