04feb5dcda6899b740ba53c060aaa32bec6f7cc5
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2020-03-07 21:41:30 +1100

    Return slices from simple file implementations

--- codespan-reporting/src/files.rs
+++ codespan-reporting/src/files.rs
@@ -204,7 +204,7 @@ where
 {
     type FileId = ();
     type Origin = Origin;
-    type LineSource = String;
+    type LineSource = &'a str;
 
     fn origin(&self, (): ()) -> Option<Origin> {
         Some(self.origin.clone())
@@ -217,13 +217,13 @@ where
         }
     }
 
-    fn line(&self, (): (), line_index: usize) -> Option<Line<String>> {
+    fn line(&self, (): (), line_index: usize) -> Option<Line<&str>> {
         let range = self.line_range(line_index)?;
 
         Some(Line {
             start: range.start,
             number: line_index + 1,
-            source: self.source.as_ref()[range].to_owned(),
+            source: &self.source.as_ref()[range],
         })
     }
 }
@@ -268,7 +268,7 @@ where
 {
     type FileId = usize;
     type Origin = Origin;
-    type LineSource = String;
+    type LineSource = &'a str;
 
     fn origin(&self, file_id: usize) -> Option<Origin> {
         Some(self.get(file_id)?.origin().clone())
@@ -278,7 +278,7 @@ where
         self.get(file_id)?.line_index((), byte_index)
     }
 
-    fn line(&self, file_id: usize, line_index: usize) -> Option<Line<String>> {
+    fn line(&self, file_id: usize, line_index: usize) -> Option<Line<&str>> {
         self.get(file_id)?.line((), line_index)
     }
 }
