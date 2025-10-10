725089208a8eec0379922bb3cd46fa0d5de120f7
Ben <kaleidawave@gmail.com>
2025-10-10 12:06:26 +0100

    Fixes to readme example

--- codespan-reporting/examples/readme_preview.rs
+++ codespan-reporting/examples/readme_preview.rs
@@ -10,46 +10,51 @@
 use codespan_reporting::diagnostic::{Diagnostic, Label, LabelStyle, Severity};
 use codespan_reporting::files::SimpleFile;
 use codespan_reporting::term::{self, Config};
-
 use codespan_reporting::term::{GeneralWrite, GeneralWriteResult};
 
 #[cfg(feature = "termcolor")]
 use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
 
-#[derive(Debug)]
-pub enum Opts {
-    /// Render SVG output
-    Svg,
-    /// Render Stderr output
-    #[cfg(feature = "termcolor")]
-    Stderr {
-        /// Configure coloring of output
-        color: ColorChoice,
-    },
+#[cfg(not(feature = "std"))]
+fn main() {
+    panic!("example requires std feature");
 }
 
-fn parse_args() -> Result<Opts, pico_args::Error> {
-    let mut pargs = pico_args::Arguments::from_env();
-    match pargs.subcommand()? {
-        Some(value) => match value.as_str() {
-            "svg" => Ok(Opts::Svg),
-            #[cfg(feature = "termcolor")]
-            "stderr" => {
-                let color = pargs
-                    .opt_value_from_str("--color")?
-                    .unwrap_or(ColorChoice::Auto);
-                Ok(Opts::Stderr { color })
-            }
-            _ => Err(pico_args::Error::Utf8ArgumentParsingFailed {
-                value,
-                cause: "not a valid subcommand".to_owned(),
-            }),
-        },
-        None => Err(pico_args::Error::MissingArgument),
-    }
-}
-
+#[cfg(feature = "std")]
 fn main() -> anyhow::Result<()> {
+    #[derive(Debug)]
+    pub enum Opts {
+        /// Render SVG output
+        Svg,
+        /// Render Stderr output
+        #[cfg(feature = "termcolor")]
+        Stderr {
+            /// Configure coloring of output
+            color: ColorChoice,
+        },
+    }
+
+    fn parse_args() -> Result<Opts, pico_args::Error> {
+        let mut pargs = pico_args::Arguments::from_env();
+        match pargs.subcommand()? {
+            Some(value) => match value.as_str() {
+                "svg" => Ok(Opts::Svg),
+                #[cfg(feature = "termcolor")]
+                "stderr" => {
+                    let color = pargs
+                        .opt_value_from_str("--color")?
+                        .unwrap_or(ColorChoice::Auto);
+                    Ok(Opts::Stderr { color })
+                }
+                _ => Err(pico_args::Error::Utf8ArgumentParsingFailed {
+                    value,
+                    cause: "not a valid subcommand".to_owned(),
+                }),
+            },
+            None => Err(pico_args::Error::MissingArgument),
+        }
+    }
+
     let file = SimpleFile::new(
         "FizzBuzz.fun",
         unindent::unindent(
@@ -94,21 +99,22 @@ fn main() -> anyhow::Result<()> {
 
     match parse_args()? {
         Opts::Svg => {
-            let mut buffer = Vec::new();
-            let mut writer = SvgWriter::new(&mut buffer);
+            let mut writer = SvgWriter::new();
             let config = Config::default();
 
             for diagnostic in &diagnostics {
                 term::emit_to_write_style(&mut writer, &config, &file, diagnostic)?;
             }
 
-            let num_lines = buffer.iter().filter(|byte| **byte == b'\n').count() + 1;
+            let num_lines = writer.line_count();
 
             let padding = 10;
             let font_size = 12;
             let line_spacing = 3;
             let width = 882;
             let height = padding + num_lines * (font_size + line_spacing) + padding;
+
+            let content = writer.into_string();
 
             let stdout = std::io::stdout();
             let writer = &mut stdout.lock();
@@ -191,22 +197,12 @@ fn main() -> anyhow::Result<()> {
 
   <foreignObject x="0" y="0" width="{width}" height="{height}">
     <div xmlns="http://www.w3.org/1999/xhtml">
-      <pre>"#,
-                padding = padding,
-                font_size = font_size,
-                width = width,
-                height = height,
-            )?;
-
-            writer.write_all(&buffer)?;
-
-            write!(
-                writer,
-                "</pre>
+      <pre>
+        {content}
+      </pre>
     </div>
   </foreignObject>
-</svg>
-"
+</svg>"#
             )?;
         }
         #[cfg(feature = "termcolor")]
@@ -222,23 +218,51 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-pub struct SvgWriter<W> {
-    upstream: W,
+// This whole example requires the std feature, but below is feature agnostic for reference
+
+#[cfg(feature = "std")]
+type WriterBuffer = Vec<u8>;
+
+#[cfg(not(feature = "std"))]
+type WriterBuffer = String;
+
+pub struct SvgWriter {
+    buffer: WriterBuffer,
     span_open: bool,
 }
 
-impl<W: GeneralWrite> SvgWriter<W> {
-    pub fn new(upstream: W) -> Self {
+impl SvgWriter {
+    pub fn new() -> Self {
         SvgWriter {
-            upstream,
+            buffer: WriterBuffer::default(),
             span_open: false,
         }
+    }
+
+    #[cfg(feature = "std")]
+    pub fn into_string(self) -> String {
+        String::from_utf8(self.buffer).unwrap()
+    }
+
+    #[cfg(not(feature = "std"))]
+    pub fn into_string(self) -> String {
+        self.buffer
+    }
+
+    #[cfg(feature = "std")]
+    pub fn line_count(&self) -> usize {
+        self.buffer.iter().filter(|byte| **byte == b'\n').count() + 1
+    }
+
+    #[cfg(not(feature = "std"))]
+    pub fn line_count(&self) -> usize {
+        self.buffer.lines().count()
     }
 
     /// Close any open span
     fn close_span(&mut self) -> GeneralWriteResult {
         if self.span_open {
-            write!(self.upstream, "</span>")?;
+            write!(self.buffer, "</span>")?;
             self.span_open = false;
         }
         Ok(())
@@ -248,14 +272,14 @@ impl<W: GeneralWrite> SvgWriter<W> {
     fn open_span(&mut self, class: &str) -> GeneralWriteResult {
         // close existing first
         self.close_span()?;
-        write!(self.upstream, "<span class=\"{}\">", class)?;
+        write!(self.buffer, "<span class=\"{}\">", class)?;
         self.span_open = true;
         Ok(())
     }
 }
 
 #[cfg(feature = "std")]
-impl<W: std::io::Write> std::io::Write for SvgWriter<W> {
+impl std::io::Write for SvgWriter {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         let mut last = 0;
         for (i, &b) in buf.iter().enumerate() {
@@ -265,41 +289,41 @@ impl<W: std::io::Write> std::io::Write for SvgWriter<W> {
                 b'&' => b"&amp;"[..].as_ref(),
                 _ => continue,
             };
-            self.upstream.write_all(&buf[last..i])?;
-            self.upstream.write_all(escape)?;
+            self.buffer.write_all(&buf[last..i])?;
+            self.buffer.write_all(escape)?;
             last = i + 1;
         }
-        self.upstream.write_all(&buf[last..])?;
+        self.buffer.write_all(&buf[last..])?;
         Ok(buf.len())
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
-        self.upstream.flush()
+        self.buffer.flush()
     }
 }
 
 #[cfg(not(feature = "std"))]
-impl<W: core::fmt::Write> core::fmt::Write for SvgWriter<W> {
+impl core::fmt::Write for SvgWriter {
     fn write_str(&mut self, s: &str) -> Result<(), core::fmt::Error> {
         let mut last = 0;
         // TODO match indices
-        for (i, &b) in s.chars().enumerate() {
+        for (i, b) in s.chars().enumerate() {
             let escape = match b {
                 '<' => "&lt;",
                 '>' => "&gt;",
                 '&' => "&amp;",
                 _ => continue,
             };
-            self.upstream.write_str(&buf[last..i])?;
-            self.upstream.write_str(escape)?;
+            self.buffer.write_str(&s[last..i])?;
+            self.buffer.write_str(escape)?;
             last = i + 1;
         }
-        self.upstream.write_str(&buf[last..])?;
-        Ok(buf.len())
+        self.buffer.write_str(&s[last..])?;
+        Ok(())
     }
 }
 
-impl<W: GeneralWrite> codespan_reporting::term::WriteStyle for SvgWriter<W> {
+impl codespan_reporting::term::WriteStyle for SvgWriter {
     fn set_header(&mut self, severity: Severity) -> GeneralWriteResult {
         let class = match severity {
             Severity::Bug => "header-bug",
